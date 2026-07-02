2891f6106bff3af70856f018565edb754ec843a5
Rude_Sloth <rudesloth@gmail.com>
2026-07-02 20:27:40 +0900

    popup test 0702_2

--- public/js/vworld-map.js
+++ public/js/vworld-map.js
@@ -5,7 +5,7 @@
     //지도 초기위치, 초기확대 설정
     vw.ol3.CameraPosition.center = [14135322.3133077,4514170.2785697]; //서울 중심 (EPSG:3857)
     //[14328808.6, 4191880.7]; //부산 중심 (EPSG:3857)
-    vw.ol3.CameraPosition.zoom = 9;
+    vw.ol3.CameraPosition.zoom = 11;
 
     vw.ol3.MapOptions = {
         basemapType: vw.ol3.BasemapType.GRAPHIC,//PHOTO_HYBRID, 지도 종류 설정
@@ -23,8 +23,11 @@
         x : 14126754.2858722,
         y : 4519757.4390882,
         epsg : "EPSG:3857",//EPSG:3857 기반 좌표
-        title : '불광천 증산교',
-        contents : '불광천 증산교 위치입니다.',
+        title: '증산교 (불광천)',
+        contents: "현재 수위 정보 보기<br><a href='https://www.riverlevel-info.kr/seoul/1401' target='_blank'>▶ 상세 페이지로 이동</a>",
+        watgCd: '1401',
+//        title : '불광천 증산교',
+//        contents : '불광천 증산교 위치입니다.',
         iconUrl : href="js/pin2.png",
         text : {
             offsetX: -20,
@@ -51,6 +54,25 @@
             fill: {color: '#000'},//글자색 설정
             stroke: {color: '#fff', width: 4},//글자 테두리 설정
             text: '홍제천 사천교'
+        }
+    };
+    markerLayer.addMarker(vw.ol3.markerOption);
+    
+    
+    vw.ol3.markerOption = {
+        x : 14127723.3523401,
+        y : 4518246.2154397,
+        epsg : "EPSG:3857",//EPSG:3857 기반 좌표
+        title : '홍제천 성산2교',
+        contents : '홍제천 성산2교 위치입니다.',
+        iconUrl : href="js/pin2.png",
+        text : {
+            offsetX: -20,
+            offsetY: 12,//핀 위치 설정
+            font: '15px Calibri,sans-serif',//글자 크기 및 폰트 설정
+            fill: {color: '#000'},//글자색 설정
+            stroke: {color: '#fff', width: 4},//글자 테두리 설정
+            text: '홍제천 성산2교'
         }
     };
     markerLayer.addMarker(vw.ol3.markerOption);
