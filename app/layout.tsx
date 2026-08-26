a88d1dc9af5b7ed06b2708eb19a227d7f522c3c7
Rude_Sloth <rudesloth@gmail.com>
2026-08-27 01:28:59 +0900

    fixed error about korean font by local bundling, from CDN

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,10 +4,17 @@ import { SpeedInsights } from "@vercel/speed-insights/next"
 
 import Script from "next/script";
 import type { Metadata } from 'next';
+import localFont from 'next/font/local';
 import './globals.css';
 
 import { SidebarProvider } from '@/components/SidebarContext';
 
+const pretendard = localFont({
+  src: '../public/PretendardVariable.woff2',
+  variable: '--font-pretendard',
+  weight: '45 920', // variable font의 가변 굵기 범위
+  display: 'swap',
+});
 
 export const metadata: Metadata = {
   title: '실시간 하천 수위 현황',
@@ -17,15 +24,14 @@ export const metadata: Metadata = {
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="ko">
+    <html lang="ko" className={pretendard.variable}>
       <head>
       </head>
+
       <body>
-        
         <SidebarProvider>
           {children}
         </SidebarProvider>
-
         <Analytics />
         <SpeedInsights />
       </body>
